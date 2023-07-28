b46fcb419fcee82fbb8be10697f0f408e7475195
Milton Honji <factorysolutionIT@outlook.com>
2023-07-28 16:41:42 -0300

    Um pouco de Razor

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -5,8 +5,8 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }       
-        [DataType (DataType.Date)]
+        public string? Title { get; set;}
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
         public decimal Price { get; set; }
