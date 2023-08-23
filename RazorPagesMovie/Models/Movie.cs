d01191033b4c97d5f35ae7a37707ae9d0e90db95
Milton Honji <factorysolutionIT@outlook.com>
2023-08-23 12:26:46 -0300

    Adicionando novos arquivos

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,12 +6,12 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set;}
+        public string Title { get; set;} = string.Empty;
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+        public string Genre { get; set; } = string.Empty;
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
