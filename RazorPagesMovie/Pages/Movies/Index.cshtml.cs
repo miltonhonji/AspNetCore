bace01d26bf788d5516feea36c793fbea59749e2
Milton Honji <factorysolutionIT@outlook.com>
2023-08-24 17:52:33 -0300

    Adicionando o campo de pesquisa

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -14,6 +16,14 @@ namespace RazorPagesMovie.Pages_Movies
         }
 
         public IList<Movie> Movie { get; set; } = default!;
+
+        [BindProperty(SupportsGet =true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
